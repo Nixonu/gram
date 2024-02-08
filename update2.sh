ed6984c84aa651311ca2ad5edcd474bd18368db5
Nixonu <nixonu@ya.ru>
2024-02-08 16:27:08 +0500

    Update update2.sh

--- update2.sh
+++ update2.sh
@@ -18,11 +18,6 @@ gpu_stats=$(< $GPU_STATS_JSON)
  
 #Start miner
 npm install
-gpu_stats=$(< $GPU_STATS_JSON)
-	readarray -t gpu_stats < <( jq --slurp -r -c '.[] | .busids, .brand, .temp, .fan | join(" ")' $GPU_STATS_JSON 2>/dev/null)
-	busids=(${gpu_stats[0]})
-#	gpu_count=${#busids[@]}
-	gpu_count=`nvtool --gpucount | grep "GPU count:" | awk '{gsub(/[^0-9]/,""); print}'`
+gpu_count=`nvtool --gpucount | grep "GPU count:" | awk '{gsub(/[^0-9]/,""); print}'`
 screen -dmS meridian bash -c "while true; do /usr/bin/node ~/JettonGramGpuMiner/send_multigpu_meridian.js --api tonapi --givers 1000 --bin ~/JettonGramGpuMiner/pow-miner-cuda --gpu-count $gpu_count; sleep 1; done"
-	
  screen -ls
