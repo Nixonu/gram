6df15d200accd7618dee028faa2ee6b53dea12f1
Nixonu <nixonu@ya.ru>
2024-04-11 09:14:48 +0500

    Create forton.sh

---
+++ forton.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update && apt install tmux -y && apt install libc6 -y
+apt install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt update
+apt install -y nodejs
+sudo apt install npm -y
